c5d5ed412fc1c241e20a80e5de3634be103b4438
JRebertt <joaorebertt0219@gmail.com>
2023-05-25 09:54:45 -0300

    finished experience section

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -4,21 +4,21 @@ import { motion } from "framer-motion";
 
 export function ExperienceCard() {
   return (
-    <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px]  md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden">
+    <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] mt-24 md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden">
       <motion.img
         initial={{ y: -100, opacity: 0 }}
-        whileInView={{ opacity: 1 }}
+        whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 1.2 }}
         viewport={{ once: true }}
-        className="w-24 h-24 rounded-full xl:w-[200px] xl:h-[200px] object-cover"
+        className="w-24 h-24 rounded-full xl:w-[100px] xl:h-[100px] object-cover object-center"
         src="https://picsum.photos/200/200"
         alt=""
       />
       <div className="px-0 md:px-10">
-        <h4 className="text-4xl font-light">CEO of Something</h4>
+        <h4 className="text-3xl font-light">CEO of Something</h4>
         <p className="font-bold text-2xl mt-1">Something</p>
 
-        <div className=" flex space-x-4 ml-5 text-lg">
+        <div className=" flex space-x-2 my-2 ">
           {/* Tech used */}
           {/* Tech used */}
           {/* Tech used */}
--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -6,7 +6,7 @@ export function WorkExperience() {
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Experience
       </h3>
-      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory">
+      <div className="w-full flex space-x-5 p-4 overflow-x-scroll snap-x snap-mandatory">
         <ExperienceCard />
         <ExperienceCard />
       </div>
