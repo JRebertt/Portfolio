9162c9ffb684052e439bc0ed8bb6f6389705f969
JRebertt <joaorebertt0219@gmail.com>
2023-05-30 17:26:03 -0300

    fineshed skiils

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { About } from "@/components/About";
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
+import { Projects } from "@/components/Projects";
+import { Skills } from "@/components/Skills";
 import { WorkExperience } from "@/components/WorkExperience";
 
 export default function Home() {
@@ -18,6 +20,14 @@ export default function Home() {
 
       <section id="experience" className="snap-center">
         <WorkExperience />
+      </section>
+
+      <section id="skills" className="snap-start">
+        <Skills />
+      </section>
+
+      <section id="projects" className="snap-start">
+        <Projects />
       </section>
     </div>
   );
--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -3,7 +3,7 @@ import { ExperienceCard } from "./ExperienceCard";
 export function WorkExperience() {
   return (
     <div className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center">
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-20 uppercase tracking-[20px] text-gray-500 text-2xl">
         Experience
       </h3>
       <div className="w-full flex space-x-5 p-4 overflow-x-scroll snap-x snap-mandatory">
